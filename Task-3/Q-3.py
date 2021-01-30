8ee13d61cd90d40a33d6610df468635606ffcd5f
Padmaja Ramesh <62902780+padmaja125@users.noreply.github.com>
2021-01-30 16:11:12 -0500

    Updated file

--- Task-3/Q-3.py
+++ Task-3/Q-3.py
@@ -8,3 +8,20 @@ print (y)
 result1 = reduce((lambda x, y: x * y), list)
 print(result1)
 
+# Method-2
+list=[18,20,30,70,90]
+a=0;
+b=1;
+for i in range(len(list)):
+    a = a+ list[i] # addition of all numbers in list
+    b = b*list[i] #multiplication of all numbers in list
+print("Sum: ",a)
+print("Multiplication: ",b)
+
+# Method-3
+from functools import reduce
+List=[18, 20, 30, 70, 90]
+addition = reduce((lambda a,b:a+b), List)
+print(addition)
+multiplication = reduce((lambda a,b:a*b), List)
+print(multiplication)
