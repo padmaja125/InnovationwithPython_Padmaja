e2926c2e22d085ec2dff59e6ff00094691696a70
Padmaja Ramesh <62902780+padmaja125@users.noreply.github.com>
2021-01-31 19:30:20 -0500

    updated

--- Task-3/Q-8.py
+++ Task-3/Q-8.py
@@ -4,3 +4,18 @@ c = {}
 c.update(a)
 c.update(b)
 print(c)
+
+# method-2
+a = {1:10,2:20}
+b = {3:30,4:40}
+d = {}
+for c in (a,b):
+    d.update(c)
+print(d)
+
+# method-3
+a = {1 : 10, 2 : 20}
+b = {3 : 30, 4 : 40}
+c = {**a,**b} # **kwargs allows you to pass keyworded variable length of arguments to a function
+print(c)
+
