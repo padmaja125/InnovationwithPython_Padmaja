b75c64a6301c52845c93e8498c09c6087b396082
Padmaja Ramesh <62902780+padmaja125@users.noreply.github.com>
2021-01-31 19:42:30 -0500

    updated

--- Task-5/Q-1.py
+++ Task-5/Q-1.py
@@ -1,10 +1,17 @@
-"""
+
 try:
     print eval('six times seven')
 except SyntaxError, err:
     print ('Syntax error', err)
-"""
+
+# method-2
 try:
     eval('x === x')
 except SyntaxError:
     print ("You cannot do that")
+    
+ # method-3
+try:
+    eval('a <> b')
+except SyntaxError:
+    print("Syntax Error")
