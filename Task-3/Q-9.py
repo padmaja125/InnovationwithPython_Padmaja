d0d6ab5f9cb30014fd09c27624841f378fa67996
Padmaja Ramesh <62902780+padmaja125@users.noreply.github.com>
2021-01-31 19:33:53 -0500

    changes updated

--- Task-3/Q-9.py
+++ Task-3/Q-9.py
@@ -6,3 +6,11 @@ def create_dict(n):
     return a_dict
 create_dict(5)
 print (a_dict)
+
+# method-2
+n=int(input("Enter any number: "))
+a= dict()
+for x in range(1,n+1):
+    a[x]=x*x # a[x]=x**2
+print(a)
+
